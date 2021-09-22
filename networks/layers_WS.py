5323fa0d5110c7cb8cddf6cb70ec44aaee9a0133
Marco Forte <fortemarco.irl@gmail.com>
2021-09-22 15:40:01 +0000

    update notebook

--- networks/layers_WS.py
+++ networks/layers_WS.py
@@ -31,26 +31,3 @@ class Conv2d(nn.Conv2d):
 
 def norm(dim):
     return nn.GroupNorm(32, dim)
-
-
-class CustomGroupNorm(torch.nn.Module):
-    def __init__(self, num_groups, num_channels, eps=1e-5, affine=True):
-        super().__init__()
-        self.num_groups = num_groups
-        self.eps = eps
-        self.num_channels = num_channels
-        self.weight = nn.Parameter(torch.randn(num_channels)).float()
-        self.bias = nn.Parameter(torch.randn(num_channels)).float()
-        
-
-    def forward(self, imput):
-        b, _, _, _ = imput.size()
-        x = imput.view(b, self.num_groups, -1)
-        mean = torch.mean(x, dim=-1, keepdim=True)
-        x = x - mean
-        std = (torch.mean(torch.pow(x,2), dim=-1, keepdim=True) + self.eps).sqrt()
-        x = x / std
-
-        x = x.view(imput.size())
-        x = x*self.weight.view(1,self.num_channels,1,1) + self.bias.view(1,self.num_channels,1,1)
-        return x
